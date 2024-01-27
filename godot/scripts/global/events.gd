9f23e10e2c36f70302f0c74aa2a7800d7ab348b8
MagikBased <124455610+MagikBased@users.noreply.github.com>
2024-01-27 10:07:17 -0600

    added some starting resources

---
+++ godot/scripts/global/events.gd
@@ -0,0 +1,15 @@
+extends Node
+
+signal card_drag_started(card_ui: CardUI)
+signal card_drag_ended(card_ui: CardUI)
+
+
+signal player_hand_drawn
+signal player_turn_ended
+signal player_hit
+signal player_died
+
+signal enemy_died
+signal enemy_action_completed
+signal enemy_turn_ended
+
